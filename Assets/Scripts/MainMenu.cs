f6537e53ce0320d30c4e74b0aba207f1a5edc674
Jonathan Gomez <gomez.jon480@gmail.com>
2024-01-26 21:04:50 -0700

    Add in relay system
    This supports the ability to play peer-to-peer across firewall and
    without port-forwarding. Users will host a game, receive a join code,
    and users will use that code to join a specific game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,28 +9,61 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] RelayManager relay;
     [SerializeField] Button hostButton;
     [SerializeField] Button serverButton;
     [SerializeField] Button clientButton;
     [SerializeField] Button startButton;
     [SerializeField] GameObject clientConnectingMessage;
     [SerializeField] GameObject clientWaitMessage;
+    [SerializeField] GameObject serverCreatingMessage;
     [SerializeField] TextMeshProUGUI serverClientsConnectedText;
+    [SerializeField] TMP_InputField joinCodeInput;
+    [SerializeField] TMP_InputField joinCodeText;
 
     void Start()
     {
         hostButton.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartHost();
+            if (relay.UsingRelay())
+            {
+                relay.OnJoinCodeGenerated += (joinCode) => {
+                    EnableServerReadyInterface(joinCode);
+                };
+                relay.CreateRelay();
+            }
+            else
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+
             EnableServerInterface();
         });
 
         serverButton.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartServer();
+            if (relay.UsingRelay())
+            {
+                relay.OnJoinCodeGenerated += (joinCode) => {
+                    EnableServerReadyInterface(joinCode);
+                };
+                relay.CreateRelay(false);
+            }
+            else
+            {
+                NetworkManager.Singleton.StartServer();
+            }
+
             EnableServerInterface();
         });
 
         clientButton.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartClient();
+            if (relay.UsingRelay())
+            {
+                relay.JoinRelay(joinCodeInput.text);
+            }
+            else
+            {
+                NetworkManager.Singleton.StartClient();
+            }
             EnableClientInterface();
         });
 
@@ -44,9 +77,26 @@ public class MainMenu : MonoBehaviour
         hostButton.gameObject.SetActive(false);
         serverButton.gameObject.SetActive(false);
         clientButton.gameObject.SetActive(false);
+        joinCodeInput.gameObject.SetActive(false);
 
+        if (relay.UsingRelay())
+            serverCreatingMessage.SetActive(true);
+        else
+            EnableServerReadyInterface("");
+
+    }
+
+    private void EnableServerReadyInterface(string joinCode)
+    {
+        serverCreatingMessage.SetActive(false);
         startButton.gameObject.SetActive(true);
         serverClientsConnectedText.gameObject.SetActive(true);
+
+        if (relay.UsingRelay())
+        {
+            joinCodeText.gameObject.SetActive(true);
+            joinCodeText.text = joinCode;
+        }
 
         NetworkManager.Singleton.OnClientConnectedCallback += (clientId) => {
             Debug.Log($"[SERVER] Client connected (client id: {clientId})");
@@ -59,6 +109,7 @@ public class MainMenu : MonoBehaviour
         hostButton.gameObject.SetActive(false);
         serverButton.gameObject.SetActive(false);
         clientButton.gameObject.SetActive(false);
+        joinCodeInput.gameObject.SetActive(false);
 
         clientConnectingMessage.SetActive(true);
         NetworkManager.Singleton.OnClientConnectedCallback += (clientId) => {
---
+++ Assets/Scripts/RelayManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RelayManager : MonoBehaviour
+{
+    [Header("Debug")]
+    [SerializeField] bool useRelay = true;
+
+    public event System.Action<string> OnJoinCodeGenerated;
+
+    // Start is called before the first frame update
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log($"Signed in as {AuthenticationService.Instance.PlayerId}");
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async void CreateRelay(bool host = true)
+    {
+        try
+        {
+            Allocation allocation =  await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log($"[SERVER] Created relay with code {joinCode}");
+
+            RelayServerData serverData = new(allocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(serverData);
+
+            if (host)
+                NetworkManager.Singleton.StartHost();
+            else
+                NetworkManager.Singleton.StartServer();
+
+            OnJoinCodeGenerated?.Invoke(joinCode);
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    public async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            Debug.Log($"[CLIENT] Joining relay with code {joinCode}");
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData serverData = new(joinAllocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(serverData);
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    public bool UsingRelay() => useRelay;
+}
