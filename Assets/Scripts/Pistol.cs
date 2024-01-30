20b9945c94cf47717247c3dfb0533f995a8bbd56
Jonathan Gomez <gomez.jon480@gmail.com>
2024-01-29 18:53:41 -0700

    Add in base weapon class for weapons to inherit from
    Base weapon holds values such as fire rate, damage, automatic or not,
    recoil values and so on. Pistol inherits from this class and implements
    the OnFire() method. However this is doing the damage raycast which may
    need to be extracted out into its own projectile type

--- Assets/Scripts/Pistol.cs
+++ Assets/Scripts/Pistol.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Pistol : NetworkBehaviour
+public class Pistol : Weapon
 {
-    const int LEFT_MOUSE_BUTTON = 0;
-    const int RIGHT_MOUSE_BUTTON = 1;
     const float MAX_DISTANCE = 100f;
 
+    [Header("Pistol")]
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] Transform firePoint;
     [SerializeField] Transform muzzle;
@@ -17,16 +16,7 @@ public class Pistol : NetworkBehaviour
     [SerializeField] GameObject muzzleFlash;
     [SerializeField] PlayerCamera playerCamera;
 
-    [SerializeField] float damage = 10f;
-    [SerializeField] float fireRate = 0.1f;
-    [SerializeField] float bloomPerShotPercent = 0.1f;
-
-    [SerializeField] float recoilVerticalAmount = 0.1f;
-    [SerializeField] float recoilRecoveryRate = 0.1f;
-    [SerializeField] float recoilHorizontalAmount = 0.1f;
-
     [SerializeField] Bloom bloom;
-    bool aimedIn = false;
 
     Vector3 idlePosition;
     Vector3 recoilTargetPosition;
@@ -43,59 +33,56 @@ public class Pistol : NetworkBehaviour
         }
     }
 
-    void Start()
+    protected override void Start()
     {
+        if (!IsOwner) return;
+        base.Start();
+
         idlePosition = transform.localPosition;
         idleRotation = transform.localRotation;
         crosshair = FindObjectOfType<Crosshair>(true);
     }
 
-    void Update()
+    protected override void Update()
     {
-        if (Input.GetMouseButtonDown(RIGHT_MOUSE_BUTTON))
-        {
-            aimedIn = true;
-        }
-        else if (Input.GetMouseButtonUp(RIGHT_MOUSE_BUTTON))
-        {
-            aimedIn = false;
-        }
-
-        if (Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON))
-        {
-            Recoil();
-
-            Vector3 direction = firePoint.forward;
-            if (!aimedIn)
-            {
-                //crosshair.Bloom(bloomPerShotPercent);
-                direction = bloom.AdjustForBloom(direction);
-
-                bloom.AddBloom(bloomPerShotPercent);
-            }
-
-            Vector3 hitPosition = firePoint.position + direction * MAX_DISTANCE;
-            if (Physics.Raycast(firePoint.position, direction, out RaycastHit hit, Mathf.Infinity, layerMask))
-            {
-                hitPosition = hit.point;
-                if (hit.transform.tag == "Player")
-                {
-                   hit.transform.GetComponent<Player>().TakeDamage(damage, OwnerClientId);
-                }
-            }
-
-            SpawnFiringEffects(hitPosition);
-            SpawnFiringEffectsServerRpc(hitPosition);
-
-            recoilTargetPosition = idlePosition - firePoint.forward * Random.Range(0.1f, 0.2f);
-            recoilTargetRotation = idleRotation * Quaternion.Euler(Random.Range(-25f, -10f), 0, 0f);
-        }
+        if (!IsOwner) return;
+        base.Update();
 
         transform.localPosition = Vector3.Lerp(transform.localPosition, recoilTargetPosition, Time.deltaTime * 20f);
         recoilTargetPosition = Vector3.Lerp(recoilTargetPosition, idlePosition, Time.deltaTime * 10f);
 
         transform.localRotation = Quaternion.Lerp(transform.localRotation, recoilTargetRotation, Time.deltaTime * 20f);
         recoilTargetRotation = Quaternion.Lerp(recoilTargetRotation, idleRotation, Time.deltaTime * 10f);
+    }
+
+    protected override void OnFire()
+    {
+        Recoil();
+
+        Vector3 direction = firePoint.forward;
+        if (!IsAimedIn())
+        {
+            //crosshair.Bloom(bloomPerShotPercent);
+            direction = bloom.AdjustForBloom(direction);
+
+            bloom.AddBloom(GetBloom());
+        }
+
+        Vector3 hitPosition = firePoint.position + direction * MAX_DISTANCE;
+        if (Physics.Raycast(firePoint.position, direction, out RaycastHit hit, Mathf.Infinity, layerMask))
+        {
+            hitPosition = hit.point;
+            if (hit.transform.tag == "Player")
+            {
+                hit.transform.GetComponent<Player>().TakeDamage(GetDamage(), OwnerClientId);
+            }
+        }
+
+        SpawnFiringEffects(hitPosition);
+        SpawnFiringEffectsServerRpc(hitPosition);
+
+        recoilTargetPosition = idlePosition - firePoint.forward * Random.Range(0.1f, 0.2f);
+        recoilTargetRotation = idleRotation * Quaternion.Euler(Random.Range(-25f, -10f), 0, 0f);
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -121,8 +108,9 @@ public class Pistol : NetworkBehaviour
 
     private void Recoil()
     {
-        float x = Random.Range(-recoilHorizontalAmount, recoilHorizontalAmount);
-        float y = recoilVerticalAmount;
+        (float horizontal, float vertical) = GetRecoil();
+        float x = Random.Range(-horizontal, horizontal);
+        float y = vertical;
         playerCamera.Rotate(x, y);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Profiling;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    const int LEFT_MOUSE_BUTTON = 0;
+    const int RIGHT_MOUSE_BUTTON = 1;
+
+    [Header("General")]
+    [SerializeField] float damage = 10f;
+    [SerializeField] bool canADS = false;
+    bool aimedIn = false;
+    [SerializeField] float fireRate = 0.1f;
+    bool canFire = true;
+    [SerializeField] bool isAutomatic = false;
+
+    [SerializeField] float bloomPerShotPercent = 0.1f;
+    [SerializeField] float recoilVerticalAmount = 0.1f;
+    [SerializeField] float recoilRecoveryRate = 0.1f;
+    [SerializeField] float recoilHorizontalAmount = 0.1f;
+
+    protected abstract void OnFire();
+
+    protected virtual void Start()
     {
-        
+        if (!IsOwner) return;
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        if (!IsOwner) return;
+
+        CheckAim();
+
+        if (CheckFire())
+        {
+            StartCoroutine(FireRateCooldown());
+            OnFire();
+        }
+    }
+
+    private void CheckAim()
+    {
+        if (!canADS) return;
+
+        if (Input.GetMouseButtonDown(RIGHT_MOUSE_BUTTON))
+        {
+            aimedIn = true;
+        }
+        else if (Input.GetMouseButtonUp(RIGHT_MOUSE_BUTTON))
+        {
+            aimedIn = false;
+        }
+    }
+
+    private bool CheckFire()
+    {
+        if (!canFire) return false;
+
+        if (isAutomatic && Input.GetMouseButton(LEFT_MOUSE_BUTTON))
+        {
+            return true;
+        }
+        else if (!isAutomatic && Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool IsAimedIn() => aimedIn;
+    public float GetDamage() => damage;
+    public float GetBloom() => bloomPerShotPercent;
+    public (float horizontal, float vertical) GetRecoil() => (horizontal: recoilVerticalAmount, vertical: recoilHorizontalAmount);
+
+    IEnumerator FireRateCooldown()
+    {
+        canFire = false;
+        yield return new WaitForSeconds(fireRate);
+        canFire = true;
     }
 }
