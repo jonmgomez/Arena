7d1750eff16d341fbf4fe1ae6a954d2d59efd2d4
Jonathan Gomez <gomez.jon480@gmail.com>
2024-02-19 23:25:14 -0700

    Write to log file
    Basic writing to a log.txt file when logging

--- Assets/Scripts/Utility/Logger.cs
+++ Assets/Scripts/Utility/Logger.cs
@@ -3,6 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class LogFile
+{
+    private static readonly string LOG_FILE = Application.dataPath + "/log.txt";
+
+    public static void WriteToFile(object message)
+    {
+        WriteToFile(ObjectToString(message));
+    }
+
+    public static void WriteToFile(string message)
+    {
+        Debug.Log("Writing to log file: " + LOG_FILE);
+        if (!System.IO.File.Exists(LOG_FILE))
+        {
+            System.IO.File.WriteAllText(LOG_FILE, "Log file created at " + DateTime.Now + Environment.NewLine);
+        }
+
+        System.IO.File.AppendAllText(LOG_FILE, message + Environment.NewLine);
+    }
+
+    private static string ObjectToString(object obj)
+    {
+        if (obj == null)
+        {
+            return "Null";
+        }
+
+        return obj.ToString();
+    }
+}
+
 public class Logger
 {
     private class DefaultLogHandler : ILogHandler
@@ -44,23 +75,30 @@ public class Logger
 
     public void Log(object message)
     {
-        logger.Log(FormatMessage(message));
+        message = FormatMessage(message);
+        logger.Log(message);
+        LogFile.WriteToFile($"INFO : {message}");
     }
 
     public void LogWarning(object message)
     {
+        message = FormatMessage(message);
         logger.LogWarning("", FormatMessage(message));
+        LogFile.WriteToFile($"WARNING : {message}");
     }
 
     public void LogError(object message)
     {
+        message = FormatMessage(message);
         logger.LogError("", FormatMessage(message));
+        LogFile.WriteToFile($"ERROR : {message}");
     }
 
     public void LogException(Exception exception)
     {
         string message = exception.Message;
         LogError($"Exception: {message}");
+        LogFile.WriteToFile($"EXCEPTION : {message}");
     }
 
     private object FormatMessage(object message)
